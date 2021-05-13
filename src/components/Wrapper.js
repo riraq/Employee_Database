e86a01dadec41709d83abff06bec949805a4f9a7
riraq <riraq@mail.usf.edu>
2021-05-13 11:24:58 -0500

    set state to contain users upon app init

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -11,8 +11,30 @@ class Wrapper extends React.Component {
     search: ""
   };
 
+  componentDidMount() {
+    this.generateUsers();
+  };
 
-  
+  generateUsers = () => {
+    API.generate()
+    .then(res => this.setState({ result: res.data }))
+    .then(console.log(this.state))
+    .catch(err => console.log(err));
+  };
+
+  // handleInputChange = event => {
+  //   const value = event.target.value;
+  //   const name = event.target.name;
+  //   this.setState({
+  //     [name]: value
+  //   });
+  // };
+
+  render() {
+    return(
+    <h1>Hello</h1>
+  )}
+
 };
 
 export default Wrapper;
--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // eslint-disable-next-line
 export default {
-  search: function() {
+  generate: function() {
     return axios.get("https://randomuser.me/api/?inc=name,email,phone,cell,id,picture&nat=us&results=10");
   }
 };
