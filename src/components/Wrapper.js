79debf0c1d651e01e275e6b9c9e0aae7525bf7f3
riraq <riraq@mail.usf.edu>
2021-05-13 10:56:15 -0500

    updated API params, started Wrapper class

---
+++ src/components/Wrapper.js
@@ -0,0 +1,18 @@
+import React from "react";
+import EmployeeCard from "./EmployeeCard";
+import EmployeeList from "./EmployeeList";
+import Header from "./Header";
+import SearchForm from "./SearchForm";
+import API from "../utils/API";
+
+class Wrapper extends React.Component {
+  state = {
+    result: {},
+    search: ""
+  };
+
+
+  
+};
+
+export default Wrapper;
--- src/utils/API.js
+++ src/utils/API.js
@@ -3,6 +3,6 @@ import axios from "axios";
 // eslint-disable-next-line
 export default {
   search: function() {
-    return axios.get("https://randomuser.me/api/");
+    return axios.get("https://randomuser.me/api/?inc=name,email,phone,cell,id,picture&nat=us&results=10");
   }
 };
