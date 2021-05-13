2d994b40af412175e27fa684dd4ce9746563e8b8
riraq <riraq@mail.usf.edu>
2021-05-13 10:45:17 -0500

    added wrapper in App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
+import React from "react";
+import Wrapper from "./components/Wrapper";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return <Wrapper />;
+};
 
 export default App;
